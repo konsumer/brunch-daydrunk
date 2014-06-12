71549524057db0d1cc911b29b65b849627d567c2
David Konsumer <konsumer@jetboystudio.com>
2014-06-12 07:51:17 -0700

    modal links

--- front/js/modalLinks.js
+++ front/js/modalLinks.js
@@ -1,3 +1,5 @@
+require('./querySelectorAll');
+
 // make all rel="modal" links have no href because they trigger functions
 [].forEach.call(document.querySelectorAll('a[rel="modal"]'), function(a){
 	a.href = "";
