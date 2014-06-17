c94d4b757486d56eda55a724e6ff2c830abb0be3
David Konsumer <konsumer@jetboystudio.com>
2014-06-17 13:22:17 -0700

    added flash messages

--- server/index.js
+++ server/index.js
@@ -9,6 +9,7 @@ var express = require('express'),
     path = require('path'),
     fs = require('fs'),
     serveStatic = require('serve-static'),
+    flash= require('connect-flash'),
     logger = require('morgan');
 
 // load config
@@ -40,6 +41,27 @@ app.use(session({
         mongooseConnection: mongoose.connection
     })
 }));
+
+// flash messages
+app.use(flash());
+
+// flash message & redirect or send JSON
+app.use(function(req, res, next){
+    res.message = function(message, type, redirect){
+        type = type || 'info';
+        if (req.xhr) {
+            if (type == 'error'){
+                return res.send(500, {message:{type:type, text:message}});
+            }
+            return res.send({message:{type:type, text:message}});
+        }
+        req.flash(type, message);
+        if (redirect !== ''){
+            res.redirect(redirect || '/');
+        }
+    };
+    next();
+});
 
 //  static service
 app.use(serveStatic(path.join(__dirname, '..', 'generated')));
